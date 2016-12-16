fc03f542bf179cdf94536e53b03eb570f0265a1e
Philip <Philip.zdb@gmail.com>
2016-12-16 12:39:17 +0100

    Adds rspec test for checking: users mailbox name should eq the users name

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,10 @@ RSpec.describe User, type: :model do
       expect(FactoryGirl.create(:user)).to be_valid
     end
   end
+
+  describe 'user' do
+    it 'the mailbox name should be subject.name' do
+      expect(subject.mailboxer_name).to eq subject.name
+    end
+  end
 end
