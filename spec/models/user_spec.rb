055de2df86bfa8e6e6f39840053e92926f8f0646
Philip Zudemberg <philip.zdb@gmail.com>
2016-12-16 16:12:14 +0100

    Rspec tests for model: User (#12)
    * Adds rspec test for checking: users mailbox name should eq the users name
    
    * Adds rspec for checking if the mailboxer email is eq to the users email
    
    * Adds rspec test for checking if user class has name column in db
    
    * Adds rspec for validating the users name
    
    * Renames some tests for mailbox methods
    
    * Renames a describe to make more sense
    
    * Adds rspec for testing presence of email, and also validating uniqueness for name, refactored the code

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,28 @@ RSpec.describe User, type: :model do
       expect(FactoryGirl.create(:user)).to be_valid
     end
   end
+
+  describe 'Mailboxer methods' do
+    it '#mailbox_name should return subject.name' do
+      expect(subject.mailboxer_name).to eq subject.name
+    end
+
+    it '#mailbox_email should return subject.email' do
+      expect(subject.mailboxer_email(subject.email)).to eq subject.email
+    end
+  end
+
+  describe 'DB table' do
+    it { should have_db_column :name }
+    it { should have_db_column :email }
+  end
+
+  describe 'validations' do
+    it { should validate_presence_of :name }
+    it { should validate_presence_of :email }
+  end
+
+  describe 'validate uniqueness' do
+    it { should validate_uniqueness_of(:name) }
+  end
 end
